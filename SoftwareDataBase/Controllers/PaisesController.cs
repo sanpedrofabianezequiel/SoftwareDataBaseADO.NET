c673231c1fe63535a24a502cc289c9b95278bd49
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-11-07 21:39:40 -0300

    Agregaos la vista del Index usando _context de ADO.NET

---
+++ SoftwareDataBase/Controllers/PaisesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoftwareDataBase.Controllers
+{
+    public class PaisesController : Controller
+    {
+        Models.BibliotecaOKEntities _context = new Models.BibliotecaOKEntities();
+        // GET: Paises
+        public ActionResult Index()
+        {
+            //Linq creamos una lista de paise
+            var listaPaises = (from x in _context.Paises
+                              select x);
+
+            return View("Index",listaPaises.ToList());
+        }
+    }
+}
